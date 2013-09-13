a0197e44bdf8933c80af9bd8dad5dd98ac23175c
Lucas <lucasrossi20@gmail.com>
2013-09-13 18:00:11 -0300

    arrancando problema2

--- src/problema2/problema2.cpp
+++ src/problema2/problema2.cpp
@@ -2,4 +2,22 @@
 
 using namespace std;
 
+Matriz::Matriz(vector<enlace> v, unsigned servidores)
+		: _matriz(servidores * servidores, 0) {
+	_servidores = servidores;
+	_enlaces = v.size();
+	for (int i = 0; i < _enlaces; ++i) {
+		set(v[i]._srv1,v[i]._srv2,v[i]._costo);
+	}
+
+}
+
+void Matriz::set(unsigned fila, unsigned columna, unsigned valor) {
+	_matriz[_servidores * fila + columna] = valor;
+	_matriz[_servidores * columna + fila] = valor;
+}
+
+unsigned Matriz::get(unsigned fila, unsigned columna) {
+	return _matriz[_servidores * fila + columna];
+}
 
--- src/problema2/problema2.h
+++ src/problema2/problema2.h
@@ -1,4 +1,30 @@
 #ifndef PROBLEMA2_H
 #define PROBLEMA2_H
 
+#include <vector>
+
+struct enlace {
+	unsigned _srv1;
+    unsigned _srv2;
+    unsigned _costo;
+    enlace(unsigned srv1, unsigned srv2, unsigned costo){
+    	_srv1 = srv1;
+    	_srv2 = srv2;
+    	_costo = costo;
+    }
+};
+
+class Matriz{
+	public:
+		Matriz(vector<enlace> v, unsigned servidores);
+        void set(unsigned fila, unsigned columna, unsigned valor);
+        unsigned get(unsigned fila, unsigned columna);
+
+	private:
+		unsigned _servidores;
+    	unsigned _enlaces;
+        vector<unsigned> _matriz;
+};
+
 #endif // PROBLEMA2_H
+
