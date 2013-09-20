cdf51b64f9bfcc5d10f0916b3f76135a1d9a08dd
lucasr20 <lucasrossi20@gmail.com>
2013-09-20 12:10:03 -0300

    problema1 en O(n^2)

--- src/problema1/problema1.cpp
+++ src/problema1/problema1.cpp
@@ -6,41 +6,39 @@ using namespace std;
 //TM1 -> tarea actual en la máquina 1
 //TM2 -> tarea actual en la máquina 2
 int cost(int Ti, int TM1, int TM2, int cantTareas, 
-		vector<vector<vector<int> > > &arrayMemoizacion,
+		vector<vector<int> > &arrayMemoizacion,
 		vector<vector<int> > &tablaCostos, vector<int> &tareasM1) {
  	//Si me paso de la cantidad de tareas, el resultado es 0
 	if(Ti == cantTareas) {
 		return 0;
 	}
 	//Si no, me fijo si ya computé este resultado (MEMOIZACION)
-	else if(arrayMemoizacion[0][TM1][TM2] != -1) {
-		return arrayMemoizacion[0][TM1][TM2];
+	else if(arrayMemoizacion[TM1][TM2] != -1) {
+		return arrayMemoizacion[TM1][TM2];
 	}
 	//Si no lo tengo computado, hago la llamada recursiva
 	else {
 		cout <<  "Hago una it" << endl;
 		int costoPonerTiM1 = cost(Ti + 1, Ti, TM2, cantTareas, arrayMemoizacion, tablaCostos, tareasM1) + tablaCostos[Ti-1][TM1];
 		int costoPonerTiM2 = cost(Ti + 1, TM1, Ti, cantTareas, arrayMemoizacion, tablaCostos, tareasM1) + tablaCostos[Ti-1][TM2];
-		arrayMemoizacion[0][TM1][TM2] = min(costoPonerTiM1,costoPonerTiM2);
+		arrayMemoizacion[TM1][TM2] = min(costoPonerTiM1,costoPonerTiM2);
 		//Agrego las tareas de la máquina 1
 		if(costoPonerTiM1 <= costoPonerTiM2) {
 			tareasM1.push_back(Ti);
 		}
-		return arrayMemoizacion[0][TM1][TM2];
+		return arrayMemoizacion[TM1][TM2];
 	}
 }
 
 pair<int, vector<int> > problema1(int cantTareas, vector<vector<int> > tablaCostos) {
-  vector<vector<vector<int> > > arrayMemoizacion;
+  vector<vector<int> >  arrayMemoizacion;
   vector<int> tareasM1;
   tareasM1.reserve(cantTareas);
   //Creo el arreglo de 3 dimensiones para guardar los resultados ya computados
   cantTareas++;
   arrayMemoizacion.resize(cantTareas);
-  for (int i = 0; i < cantTareas; ++i) {
-    arrayMemoizacion[i].resize(cantTareas);
-    for (int j = 0; j < cantTareas; ++j)
-      arrayMemoizacion[i][j].assign(cantTareas, -1);
+    for (int j = 0; j < cantTareas; ++j) {
+      arrayMemoizacion[j].assign(cantTareas, -1);
   }
 
   //Lamar a cost y ver como recorrer la tabla armada para conseguir las tareas de una máquina
