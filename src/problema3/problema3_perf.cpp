615ab308b4b9e9df496b09f773b30da12ce125ae
Leandro Lovisolo <leandro@leandro.me>
2013-10-02 20:19:52 -0300

    Formatting

--- src/problema3/problema3_perf.cpp
+++ src/problema3/problema3_perf.cpp
@@ -73,8 +73,6 @@ TEST(problema3, PerformanceInstanciasAleatorias) {
           );
 }
 
-
-
 GTEST_API_ int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
